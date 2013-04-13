ce8144bfd2e2193a41387c569a4cd10d822b6688
peterasujan <peterasujan@gmail.com>
2013-04-13 02:11:35 -0700

    Progress on first three optimization steps. Above 6 GFlops for 40x48.

--- benchmark.c
+++ benchmark.c
@@ -29,13 +29,12 @@ void sgemm_reference( int m, int n, float *A, float *C )
 int main( int argc, char **argv )
 {
   srand(time(NULL));
-
-  	for( int n = 32; n < 100; n = n+1+n/3 )
-//	for( int n = 40; n <  41; n = n+1+n/3 )
+  //	for( int n = 32; n < 100; n = n+1+n/3 )
+  	for( int n = 40; n <  41; n = n+1+n/3 )
 	{
   /* Try different m */
-  for( int m = 32; m <  100; m = m+1+m/3 )
-//for( int m = 48; m <  49; m = m+1+m/3 )
+	    //for( int m = 32; m <  100; m = m+1+m/3 )
+		  for( int m = 48; m <  49; m = m+1+m/3 )
   {
     /* Allocate and fill 2 random matrices A, C */
     float *A = (float*) malloc( (n+m) * n * sizeof(float) );
--- sgemm-small.c
+++ sgemm-small.c
@@ -1,7 +1,31 @@
+#include <stdio.h>
+#include <emmintrin.h>
+
 void sgemm( int m, int n, int d, float *A, float *C )
 {
-  for( int i = 0; i < n; i++ )
-    for( int k = 0; k < m; k++ ) 
-      for( int j = 0; j < n; j++ ) 
-	C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
+    __m128 first;
+    __m128 second;
+    __m128 result;
+    __m128 current;
+    float *trans;
+    float *c;
+    for( int j = 0; j < n; j++ ) {
+	for( int k = 0; k < m; k++ ) {
+	    trans = &A[j*(n+1)+k*n];
+	    second = _mm_load1_ps(trans);
+	    for( int i = 0; i < n/8*8; i += 8) {
+		first = _mm_loadu_ps(&A[i+k*n]);
+		c = &C[i+j*(n)];
+		result = _mm_mul_ps(first, second);
+		_mm_storeu_ps(c, _mm_add_ps(_mm_loadu_ps(c), result));
+		first = _mm_loadu_ps(&A[i+4+k*n]);
+		c = &(C[i+4+j*(n)]);
+		result = _mm_mul_ps(first, second);
+		_mm_storeu_ps(c, _mm_add_ps(_mm_loadu_ps(c), result));
+	    }
+	    for (int i = n/8*8; i < n; i++) {
+		C[i+j*n] += A[i+k*(n)] * *trans;
+	    }
+	}
+    }
 }
